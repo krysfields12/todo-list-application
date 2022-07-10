4d456954c09bb54dc61281eb15048d85f2ccef10
Krystin Fields <krysfields12@gmail.com>
2022-07-10 10:41:35 -0400

    added styling for todo-view

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoViewComponent } from './pages/todo-view/todo-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TodoViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoViewComponent } from './pages/todo-view/todo-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoViewComponent
   ],
   imports: [
     BrowserModule,
