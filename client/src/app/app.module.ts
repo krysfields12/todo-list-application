a51143f2e5f3cfb9504ce389fde19e879c8dd416
Krystin Fields <krysfields12@gmail.com>
2022-07-10 22:55:45 -0400

    added angular routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoViewComponent } from './pages/todo-view/todo-view.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { TodoViewComponent } from './pages/todo-view/todo-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/pages/todo-view/todo-view.component.ts
+++ client/src/app/pages/todo-view/todo-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/task.service';
 
 @Component({
   selector: 'app-todo-view',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
   }
-
+  createNewTodo() {
+    this.taskService.createList("Testing", "Testing,", "7/11/2022").subscribe((response: any) => {
+      console.log(response);
+    })
+  }
 }
